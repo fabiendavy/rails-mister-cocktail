4e13aba4060c3c851c61a46195e4057fa63259a1
Fabien Davy <davyfabien@gmail.com>
2020-02-13 16:18:41 +0100

    Exercice done, rake not all green as we improved the add dose part

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "Destroying existing database..."
+Ingredient.destroy_all if Rails.env.development?
+
+puts "Creating new ingredients..."
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+
+ingredients = JSON.parse(open(url).read)
+
+ingredients["drinks"].each do |ingredient|
+  new_ingredient = Ingredient.new(
+    name: ingredient["strIngredient1"]
+  )
+  new_ingredient.save!
+  puts "#{new_ingredient.name} created!"
+end
+
+puts "All ingredients created!"
